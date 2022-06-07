164b9ef41ab927826066caa39cf0f413daf1e364
Tilicho <berserksmail@gmail.com>
2022-06-07 22:20:06 +0300

    more weird remapping

--- remap.sh
+++ remap.sh
@@ -2,8 +2,14 @@ hidutil property --set '{"UserKeyMapping":[]}'
 
 
 hidutil property --set '{"UserKeyMapping":[
-	{"HIDKeyboardModifierMappingSrc":0x7000000E5,"HIDKeyboardModifierMappingDst":0x7000000E4},
-	{"HIDKeyboardModifierMappingSrc":0x7000000E4,"HIDKeyboardModifierMappingDst":0x7000000E5},
+	{"HIDKeyboardModifierMappingSrc":0x7000000E6,"HIDKeyboardModifierMappingDst":0x7000000E4},
+	{"HIDKeyboardModifierMappingSrc":0x7000000E4,"HIDKeyboardModifierMappingDst":0x7000000E6},
+	
+	{"HIDKeyboardModifierMappingSrc":0x7000000E5,"HIDKeyboardModifierMappingDst":0x700000052},
+	{"HIDKeyboardModifierMappingSrc":0x700000052,"HIDKeyboardModifierMappingDst":0x700000051},
+	
+	{"HIDKeyboardModifierMappingSrc":0x7000000E0,"HIDKeyboardModifierMappingDst":1095216660483},
+	{"HIDKeyboardModifierMappingSrc":1095216660483,"HIDKeyboardModifierMappingDst":0x7000000E0},
 	]}'
 
 
