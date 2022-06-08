6518edd399c1b64412589518314ad38dab64e518
Tilicho <berserksmail@gmail.com>
2022-06-08 15:39:48 +0300

    trying to use magic keyb

--- remap.sh
+++ remap.sh
@@ -2,14 +2,32 @@ hidutil property --set '{"UserKeyMapping":[]}'
 
 
 hidutil property --set '{"UserKeyMapping":[
-	{"HIDKeyboardModifierMappingSrc":0x7000000E6,"HIDKeyboardModifierMappingDst":0x7000000E4},
-	{"HIDKeyboardModifierMappingSrc":0x7000000E4,"HIDKeyboardModifierMappingDst":0x7000000E6},
+			{
+              "HIDKeyboardModifierMappingDst": 0x7000000E2,
+              "HIDKeyboardModifierMappingSrc": 0x7000000E3
+            },
+            {
+              "HIDKeyboardModifierMappingDst": 0x7000000E3,
+              "HIDKeyboardModifierMappingSrc": 0x7000000E0
+            },
+            {
+              "HIDKeyboardModifierMappingDst": 0x7000000E0,
+              "HIDKeyboardModifierMappingSrc": 0xFF00000003
+            },
+            {
+              "HIDKeyboardModifierMappingDst": 0xFF00000003,
+              "HIDKeyboardModifierMappingSrc": 0x7000000E2
+            },
+            {
+              "HIDKeyboardModifierMappingDst": 0x700000052,
+              "HIDKeyboardModifierMappingSrc": 0x7000000E5
+            },
+			{
+              "HIDKeyboardModifierMappingSrc": 0x7000000E6,
+              "HIDKeyboardModifierMappingDst": 0x7000000E7
+            }
+
 	
-	{"HIDKeyboardModifierMappingSrc":0x7000000E5,"HIDKeyboardModifierMappingDst":0x700000052},
-	{"HIDKeyboardModifierMappingSrc":0x700000052,"HIDKeyboardModifierMappingDst":0x700000051},
-	
-	{"HIDKeyboardModifierMappingSrc":0x7000000E0,"HIDKeyboardModifierMappingDst":1095216660483},
-	{"HIDKeyboardModifierMappingSrc":1095216660483,"HIDKeyboardModifierMappingDst":0x7000000E0},
 	]}'
 
 
