a524d087694e833920474ae47b5090f9af0b2d7b
ser <ser@m1.local>
2022-07-19 01:31:37 +0300

    apple keyboard stuff

--- remap_for_win.sh
+++ remap_for_win.sh
@@ -16,26 +16,6 @@ hidutil property --set '{"UserKeyMapping":[]}'
 
 hidutil property --set '{"UserKeyMapping":[
 {
-    "HIDKeyboardModifierMappingDst": 0x7000000E2,
-    "HIDKeyboardModifierMappingSrc": 0x7000000E3
-},
-{
-    "HIDKeyboardModifierMappingDst": 0x7000000E3,
-    "HIDKeyboardModifierMappingSrc": 0x7000000E0
-},
-{
-    "HIDKeyboardModifierMappingDst": 0x7000000E0,
-    "HIDKeyboardModifierMappingSrc": 0xFF00000003
-},
-{
-    "HIDKeyboardModifierMappingDst": 0xFF00000003,
-    "HIDKeyboardModifierMappingSrc": 0x7000000E2
-},
-{
-    "HIDKeyboardModifierMappingDst": 0x700000052,
-    "HIDKeyboardModifierMappingSrc": 0x7000000E5
-},
-{
     "HIDKeyboardModifierMappingDst": 0x7000000E4,
     "HIDKeyboardModifierMappingSrc": 0x7000000E6
 },
