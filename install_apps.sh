0cade4d759da2a6aff1c554d15415a1d179508af
tilicho <berserksmail@gmail.com>
2023-02-24 01:36:44 +0300

    minor fix

--- install_apps.sh
+++ install_apps.sh
@@ -1,3 +1,3 @@
 apt-get update
 apt-get upgrade
-apt-get install zsh tmux ncdu neovim neofetch mc ncdu python3 cmatrix hexcurse htop tldr 
+apt-get install zsh tmux ncdu neovim neofetch mc ncdu python3 cmatrix hexcurse htop 
