f4f13297e248042a8a535a63ee36d08e1a3cef74
tilicho <tilicho@mail.com>
2025-01-24 02:26:52 +0300

    update

--- install_apps.sh
+++ install_apps.sh
@@ -3,4 +3,4 @@ apt-get upgrade
 apt-get install -y zsh tmux ncdu neovim neofetch mc ncdu python3 cmatrix hexcurse htop 
 apt-get install -y iperf3 silversearcher-ag 
 apt-get install -y exa rename coreutils fzf tldr
-apt-get install -y sysbench
+apt-get install -y sysbench avahi-daemon avahi-utils w3m
