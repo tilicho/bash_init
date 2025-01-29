b1abdc5d1ff23696c7b2be1f6c58e7dd49f1b3a8
tilicho <tilicho@mail.com>
2025-01-29 03:44:51 +0300

    ag fix

--- mac_install_apps.sh
+++ mac_install_apps.sh
@@ -1,6 +1,6 @@
 brew update
 brew upgrade
 brew install zsh tmux ncdu neovim neofetch mc ncdu python3 cmatrix hexcurse htop 
-brew install iperf3 silversearcher-ag 
+brew install iperf3 the_silver_searcher 
 brew install exa rename coreutils fzf tldr
 brew install sysbench
