568ff11d9fa50660b90b0c4b4aea126cd4926546
tilicho <tilicho@mail.com>
2025-04-11 18:40:16 +0300

    update for git

--- mac_install_apps.sh
+++ mac_install_apps.sh
@@ -1,6 +1,7 @@
 brew update
 brew upgrade
-brew install zsh tmux ncdu neovim neofetch mc ncdu python3 cmatrix hexcurse htop 
-brew install iperf3 the_silver_searcher 
+brew install zsh tmux ncdu neovim neofetch mc ncdu python3 cmatrix hexcurse htop
+brew install iperf3 the_silver_searcher
 brew install exa rename coreutils fzf tldr
 brew install sysbench macmon uv lf
+brew install ffmpeg
