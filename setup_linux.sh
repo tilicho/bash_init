16f1729535d7373a1446ce75222ec69d76026c8b
tilicho <tilicho@mail.com>
2026-04-26 15:20:27 +0300

    linux update

--- setup_linux.sh
+++ setup_linux.sh
@@ -2,3 +2,8 @@ sh install_apps.sh
 sh vim_install.sh
 cp .* ../
 chsh -s /bin/zsh
+
+curl -v --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+cargo install git-delta
+
+env CGO_ENABLED=0 go install -trimpath -ldflags="-s -w" github.com/gokcehan/lf@latest
